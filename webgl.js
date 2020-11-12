264a131ee5d901800a1cc5884577e62d4584b75d
Sofia Fateyev <pateyev.sofia@gmail.com>
2020-11-12 12:22:01 +0000

    Create three dimensional animation

--- webgl.js
+++ webgl.js
@@ -5,46 +5,101 @@ global.THREE = require("three");
 require("three/examples/js/controls/OrbitControls");
 
 const canvasSketch = require("canvas-sketch");
+const random = require("canvas-sketch-util/random");
+const palettes = require("nice-color-palettes");
+const eases = require("eases");
+const BezierEasing = require("bezier-easing");
 
 const settings = {
+  dimensions: [512, 512],
+  fps: 24,
+  duration: 4,
   // Make the loop animated
   animate: true,
   // Get a WebGL canvas rather than 2D
-  context: "webgl"
+  context: "webgl",
+
+  attributes: { antialias: true },
 };
 
 const sketch = ({ context }) => {
   // Create a renderer
   const renderer = new THREE.WebGLRenderer({
-    canvas: context.canvas
+    canvas: context.canvas,
   });
 
   // WebGL background color
-  renderer.setClearColor("#000", 1);
+  renderer.setClearColor("hsl(0, 0%, 100%)", 1);
 
   // Setup a camera
-  const camera = new THREE.PerspectiveCamera(50, 1, 0.01, 100);
-  camera.position.set(0, 0, -4);
-  camera.lookAt(new THREE.Vector3());
-
-  // Setup camera controller
-  const controls = new THREE.OrbitControls(camera, context.canvas);
+  const camera = new THREE.OrthographicCamera();
 
   // Setup your scene
   const scene = new THREE.Scene();
 
-  // Setup a geometry
-  const geometry = new THREE.SphereGeometry(1, 32, 16);
-
   // Setup a material
-  const material = new THREE.MeshBasicMaterial({
-    color: "red",
-    wireframe: true
-  });
+  const palette = random.pick(palettes);
+
+  const fragmentShader = `
+    varying vec2 vUv;
+    uniform vec3 color;
+
+    void main () {
+      gl_FragColor = vec4(vec3(color * vUv.x), 1.0);
+    }
+  `;
+
+  const vertexShader = `
+    varying vec2 vUv;
+    uniform float time;
+
+    void main () {
+      vUv = uv;
+      vec3 pos = position.xyz * sin(time);
+      gl_Position = projectionMatrix * modelViewMatrix * vec4(pos, 1.0);
+    }
+  `;
 
   // Setup a mesh with geometry + material
-  const mesh = new THREE.Mesh(geometry, material);
-  scene.add(mesh);
+  const box = new THREE.BoxGeometry(1, 1, 1);
+  const meshes = [];
+  for (let i = 0; i < 40; i++) {
+    const mesh = new THREE.Mesh(
+      box,
+      new THREE.ShaderMaterial({
+        fragmentShader,
+        vertexShader,
+        uniforms: {
+          color: { value: new THREE.Color(random.pick(palette)) },
+          time: { value: 0 },
+        },
+      })
+    );
+
+    mesh.position.set(
+      random.range(-1, 1),
+      random.range(-1, 1),
+      random.range(-1, 1)
+    );
+
+    mesh.scale.set(
+      random.range(-1, 1),
+      random.range(-1, 1),
+      random.range(-1, 1)
+    );
+
+    mesh.scale.multiplyScalar(0.5);
+    scene.add(mesh);
+    meshes.push(mesh);
+  }
+
+  scene.add(new THREE.AmbientLight("hsl(0, 0%, 10%)"));
+
+  const light = new THREE.DirectionalLight("white", 1);
+  light.position.set(0.5, 0.5, 4);
+  scene.add(light);
+
+  const easeFn = BezierEasing(0.67, 0.03, 0.29, 0.99);
 
   // draw each frame
   return {
@@ -52,19 +107,44 @@ const sketch = ({ context }) => {
     resize({ pixelRatio, viewportWidth, viewportHeight }) {
       renderer.setPixelRatio(pixelRatio);
       renderer.setSize(viewportWidth, viewportHeight, false);
-      camera.aspect = viewportWidth / viewportHeight;
+
+      const aspect = viewportWidth / viewportHeight;
+
+      // Ortho zoom
+      const zoom = 2.0;
+
+      // Bounds
+      camera.left = -zoom * aspect;
+      camera.right = zoom * aspect;
+      camera.top = zoom;
+      camera.bottom = -zoom;
+
+      // Near/Far
+      camera.near = -100;
+      camera.far = 100;
+
+      // Set position & look at world center
+      camera.position.set(zoom, zoom, zoom);
+      camera.lookAt(new THREE.Vector3());
+
+      // Update the camera
       camera.updateProjectionMatrix();
     },
     // Update & render your scene here
-    render({ time }) {
-      controls.update();
+    render({ playhead, time }) {
+      const t = Math.sin(playhead * Math.PI);
+      scene.rotation.z = easeFn(t);
+
+      meshes.forEach((mesh) => {
+        mesh.material.uniforms.time.value = time;
+      });
+
       renderer.render(scene, camera);
     },
     // Dispose of events & renderer for cleaner hot-reloading
     unload() {
-      controls.dispose();
       renderer.dispose();
-    }
+    },
   };
 };
 
